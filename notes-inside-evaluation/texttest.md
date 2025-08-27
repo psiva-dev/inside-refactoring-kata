015ac203e727ab77372b9639905c238049646179
psiva-dev <siva.prabou+webmobileappdev.com@gmail.com>
2025-08-27 12:12:30 +0200

    Cette branche réponds à la demande du GildedRoseRequirements_fr.md.
    A la demande décrite
    ```
        Nous avons récemment signé un partenariat avec un fournisseur de produit invoqué ("Conjured"). Cela nécessite une mise à jour de notre système :
    
        les éléments "Conjured" voient leur qualité se dégrader de deux fois plus vite que les objets normaux.
        Vous pouvez faire les changements que vous voulez à la méthode updateQuality et ajouter autant de code que vous voulez,
        tant que tout fonctionne correctement.
        Cependant, nous devons vous prévenir,
        vous ne devez en aucun cas modifier la classe Item ou ses propriétés car cette classe ...
    
    ```
    
    J'en ai profité pour améliorer le code en séparant les différents cas de figure avec des `if` à la manière d'un `switch case`.
    Ça évite de mélanger la gestion des différentes règles métiers selon les types de produits.
    Cela rend le code plus lisible, facile à maintenir.
    Et cela rend l'ajout de nouveaux cas plus facile à effectuer.

--- notes-inside-evaluation/texttest.md
+++ notes-inside-evaluation/texttest.md
@@ -1,3 +1,28 @@
+# original 
+
+Using local queues for Application Gilded Rose Refactoring Kata
+Q: Submitting Gilded Rose Refactoring Kata test-case ThirtyDays to default local queue
+S: Gilded Rose Refactoring Kata test-case ThirtyDays succeeded on sharingan
+
+# après dev 
+
+Using local queues for Application Gilded Rose Refactoring Kata
+Q: Submitting Gilded Rose Refactoring Kata test-case ThirtyDays to default local queue
+S: Gilded Rose Refactoring Kata test-case ThirtyDays FAILED on sharingan : differences in stdout
+View details(v), Approve(a) or continue(any other key)?
+v
+...
+
+# après approval
+
+Using local queues for Application Gilded Rose Refactoring Kata
+Q: Submitting Gilded Rose Refactoring Kata test-case ThirtyDays to default local queue
+S: Gilded Rose Refactoring Kata test-case ThirtyDays FAILED on sharingan : differences in stdout
+View details(v), Approve(a) or continue(any other key)?
+a
+Approving Gilded Rose Refactoring Kata test-case ThirtyDays 
+
+texttest -con
 Using local queues for Application Gilded Rose Refactoring Kata
 Q: Submitting Gilded Rose Refactoring Kata test-case ThirtyDays to default local queue
 S: Gilded Rose Refactoring Kata test-case ThirtyDays succeeded on sharingan
--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,38 +2,72 @@
 
 class GildedRose(object):
 
-    def __init__(self, items):
+    def __init__(self, items, default_quality_decrease_offset: int = 1):
+        """
+        Rajout de la variable default_quality_decrease_offset indiquant de combien décroit
+        par défaut la qualité d'un objet normal.
+
+        :param items:
+        :param default_quality_decrease_offset: de combien d'unité décroit la qualité d'un objet normal
+        """
         self.items = items
+        self.default_quality_decrease_offset = default_quality_decrease_offset
 
     def update_quality(self):
+        """
+        J'ai posé comme postulat que le type de produit est déterminé par le prefix de son nom.
+        Cette méthode mets à jour le qualité du produit en fonction de son type et du temps restant pour vendre le produit.
+        """
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+
+            # #########################################################################################################
+            # mise à jour de la valeur qualité
+            # #########################################################################################################
+
+            if item.name.startswith("Conjured"):
+                # La qualité se dégrade deux fois plus rapidement.
+                if item.sell_in <= 0:
+                    item.quality = max(0, item.quality - 2 * 2 * self.default_quality_decrease_offset)
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = max(0, item.quality - 2 * self.default_quality_decrease_offset)
+            elif item.name.startswith("Aged Brie"):
+                # "Aged Brie" augmente sa qualité (quality) plus le temps passe.
+                # La qualité d'un produit n'est jamais de plus de 50.
+                if item.sell_in <= 0:
+                    # Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
+                    item.quality = min(50, item.quality + 2 * self.default_quality_decrease_offset)
+                else:
+                    item.quality = min(50, item.quality + self.default_quality_decrease_offset)
+            elif item.name.startswith("Backstage"):
+                # "Backstage passes", comme le "Aged Brie", augmente sa qualité (quality) plus le temps passe (sellIn) ;
+                # - La qualité augmente de 2 quand il reste 10 jours ou moins
+                # - et de 3 quand il reste 5 jours ou moins,
+                # - mais la qualité tombe à 0 après le concert.
+                if item.sell_in <= 0:
+                    item.quality = 0
+                elif item.sell_in <= 5:
+                    item.quality =  min(50,item.quality + 3)
+                elif item.sell_in <= 10:
+                    item.quality =  min(50, item.quality + 2)
+                else:
+                    item.quality = min(50, item.quality + self.default_quality_decrease_offset)
+            elif item.name.startswith("Sulfuras"):
+                # "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (quality)
+                pass
+            else:
+                if item.sell_in <= 0:
+                    # Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
+                    item.quality = max(0, item.quality - 2 *self.default_quality_decrease_offset)
+                else:
+                    item.quality = max(0, item.quality - self.default_quality_decrease_offset)
+
+            # #########################################################################################################
+            # mise à jour de la valeur nombre de jour restant pour vendre l'article
+            # #########################################################################################################
+
+            if not item.name.startswith("Sulfuras"):
+                item.sell_in = item.sell_in - 1
+
 
 
 class Item:
